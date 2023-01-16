7e6cfa7dd74f43cbf7e1eb17c0ef3e8e2a09e00d
cpb77 <cpb77@cornell.edu>
2023-01-15 19:01:45 -0700

    added toggle button for tv show or movie search

--- backend.js
+++ backend.js
@@ -30,15 +30,27 @@ app.get('/movie/:id', (req, res) => {
   }
 });
 
-app.post('/tshirt/:id', (req, res) => {
+app.get('/tvshow/:id', (req, res) => {
 
   const { id } = req.params;
-  const { logo } = req.body;
 
-  res.status(200).send({
-    tshirt: 'test',
-    size: 'large'
-  })
+  if (!id) {
+    res.status(418).send({ message: 'id needed' });
+  } else {
+    fetch(`https://api.flixed.io/v1/shows/${id}?idType=imdb&apiKey=JvZosSdhe61qyfqx9cWtDmdng57IQHQJ`, {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+      },
+    })
+      .then(flixedRes => flixedRes.json())
+      .then((data) => {
+        res.json({ url: `${data.watchAvailability[0].directUrls[0]}`, });
+      })
+      .catch((error) => {
+        res.status(418).send({ message: error.message });
+      });
+  }
 });
 
 app.listen(
