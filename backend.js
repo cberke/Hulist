98626888176a7caf8b978cde950b9e04da13b6b5
cpb77 <cpb77@cornell.edu>
2023-03-09 23:47:59 -0500

    moved DB config into config file

--- backend.js
+++ backend.js
@@ -2,46 +2,48 @@ import express from 'express';
 import fetch from 'node-fetch';
 import cors from 'cors';
 import mysql from 'mysql2';
+import config from './config.js';
+
 
 
 const app = express();
 const PORT = 8080;
 
-let connection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'testpw123!',
-  database: 'Streamlist'
-});
+let connection = mysql.createConnection(config);
 
 app.use(cors());
 
 app.get('/movie/:title', (req, res) => {
 
-  // connection.connect(function (err) {
-  //   if (err) {
-  //     return console.error('error: ' + err.message);
-  //   }
-
-  //   let sql = 'SHOW TABLES';
-
-  //   connection.query(sql, function (err, results, fields) {
-  //     if (err) {
-  //       console.log(err.message);
-  //     }
-  //     console.log(results)
-  //   });
-
-  //   connection.end(function (err) {
-  //     if (err) {
-  //       return console.log('error:' + err.message);
-  //     }
-  //     console.log('Close the database connection.');
-  //   });
-  // });
-
   const { title } = req.params;
 
+  var url = "";
+
+  // Check if we have this URL cached
+  connection.connect(function (err) {
+    if (err) {
+      return console.error('error: ' + err.message);
+    }
+
+    let sql = `SELECT url FROM ContentMetaData WHERE name=?`;
+
+    connection.query(sql, [title], function (err, results, fields) {
+      if (err) {
+        console.log(err.message);
+      }
+      console.log(results)
+    });
+
+    connection.end(function (err) {
+      if (err) {
+        return console.log('error:' + err.message);
+      }
+      console.log('Close the database connection.');
+    });
+  });
+
+
+  // Get and cache the URL
   fetch(`http://www.omdbapi.com/?t=${title}&apikey=53dcb6b7`, {
     method: 'GET',
     headers: {
@@ -65,6 +67,32 @@ app.get('/movie/:title', (req, res) => {
         })
           .then(flixedRes => flixedRes.json())
           .then((data) => {
+            // cache the URL and associated metadata
+            // connection.connect(function (err) {
+            //   if (err) {
+            //     return console.error('error: ' + err.message);
+            //   }
+
+            //   let sql = `SELECT url FROM ContentMetaData WHERE name=?`;
+
+            //   connection.query(sql, [title], function (err, results, fields) {
+            //     if (err) {
+            //       console.log(err.message);
+            //     }
+            //     console.log(results)
+            //   });
+
+            //   connection.end(function (err) {
+            //     if (err) {
+            //       return console.log('error:' + err.message);
+            //     }
+            //     console.log('Close the database connection.');
+            //   });
+            // });
+
+
+
+            // send the data back
             res.json({ url: `${data.watchAvailability[0].directUrls[0]}`, });
           })
           .catch((error) => {
---
+++ config.js
@@ -0,0 +1,8 @@
+let config = {
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'testpw123!',
+  database: 'Streamlist'
+};
+
+export default config
