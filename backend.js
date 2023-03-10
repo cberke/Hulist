664b9013f17ec2c58cd8bd969f785355ad791928
cpb77 <cpb77@cornell.edu>
2023-03-09 23:08:01 -0500

    move omdb API access to nodeJS file, ready for caching and restricting API access to scenarios where the movie is not found in the cahce

--- backend.js
+++ backend.js
@@ -16,7 +16,7 @@ let connection = mysql.createConnection({
 
 app.use(cors());
 
-app.get('/movie/:id', (req, res) => {
+app.get('/movie/:title', (req, res) => {
 
   // connection.connect(function (err) {
   //   if (err) {
@@ -40,27 +40,41 @@ app.get('/movie/:id', (req, res) => {
   //   });
   // });
 
+  const { title } = req.params;
 
-
-  const { id } = req.params;
-
-  if (!id) {
-    res.status(418).send({ message: 'id needed' });
-  } else {
-    fetch(`https://api.flixed.io/v1/movies/${id}?idType=imdb&apiKey=JvZosSdhe61qyfqx9cWtDmdng57IQHQJ`, {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-      },
+  fetch(`http://www.omdbapi.com/?t=${title}&apikey=53dcb6b7`, {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+    },
+  })
+    .then(response => response.json())
+    .then((titleData) => {
+      var id = titleData.imdbID;
+      while (id.charAt(0) === 't') {
+        id = id.substring(1);
+      }
+      if (!id) {
+        res.status(418).send({ message: 'id needed' });
+      } else {
+        fetch(`https://api.flixed.io/v1/movies/${id}?idType=imdb&apiKey=JvZosSdhe61qyfqx9cWtDmdng57IQHQJ`, {
+          method: 'GET',
+          headers: {
+            accept: 'application/json',
+          },
+        })
+          .then(flixedRes => flixedRes.json())
+          .then((data) => {
+            res.json({ url: `${data.watchAvailability[0].directUrls[0]}`, });
+          })
+          .catch((error) => {
+            res.status(418).send({ message: error.message });
+          });
+      }
     })
-      .then(flixedRes => flixedRes.json())
-      .then((data) => {
-        res.json({ url: `${data.watchAvailability[0].directUrls[0]}`, });
-      })
-      .catch((error) => {
-        res.status(418).send({ message: error.message });
-      });
-  }
+    .catch((error) => {
+      res.status(418).send({ message: error.message });
+    });
 });
 
 app.get('/tvshow/:id', (req, res) => {
