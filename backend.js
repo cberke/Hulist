0b5e78a3aa65f3bf4875e29f0279c143e34a179f
cpb77 <cpb77@cornell.edu>
2023-03-10 20:05:24 -0700

    added caching and checking of database for urls

--- backend.js
+++ backend.js
@@ -4,12 +4,8 @@ import cors from 'cors';
 import mysql from 'mysql2';
 import config from './config.js';
 
-
-
 const app = express();
 const PORT = 8080;
-
-let connection = mysql.createConnection(config);
 
 app.use(cors());
 
@@ -20,89 +16,109 @@ app.get('/movie/:title', (req, res) => {
   var url = "";
 
   // Check if we have this URL cached
+  let connection = mysql.createConnection(config);
+
   connection.connect(function (err) {
     if (err) {
       return console.error('error: ' + err.message);
     }
-
-    let sql = `SELECT url FROM ContentMetaData WHERE name=?`;
-
-    connection.query(sql, [title], function (err, results, fields) {
-      if (err) {
-        console.log(err.message);
-      }
-      console.log(results)
-    });
-
-    connection.end(function (err) {
-      if (err) {
-        return console.log('error:' + err.message);
-      }
-      console.log('Close the database connection.');
-    });
   });
 
+  let cacheCheckSql = `SELECT url FROM ContentMetaData WHERE contentName=?`;
 
-  // Get and cache the URL
-  fetch(`http://www.omdbapi.com/?t=${title}&apikey=53dcb6b7`, {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-    },
-  })
-    .then(response => response.json())
-    .then((titleData) => {
-      var id = titleData.imdbID;
-      while (id.charAt(0) === 't') {
-        id = id.substring(1);
-      }
-      if (!id) {
-        res.status(418).send({ message: 'id needed' });
-      } else {
-        fetch(`https://api.flixed.io/v1/movies/${id}?idType=imdb&apiKey=JvZosSdhe61qyfqx9cWtDmdng57IQHQJ`, {
-          method: 'GET',
-          headers: {
-            accept: 'application/json',
-          },
+  connection.query(cacheCheckSql, [title], function (err, results, fields) {
+    if (err) {
+      console.log(err.message);
+    }
+    if (results != null) {
+
+      res.json({ url: `${results[0].url.toString()}`, });
+
+      connection.end(function (err) {
+        if (err) {
+          return console.log('error:' + err.message);
+        }
+        console.log('Close the database connection.');
+      });
+    } else {
+      // Get and cache the URL
+      fetch(`http://www.omdbapi.com/?t=${title}&apikey=53dcb6b7`, {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+        },
+      })
+        .then(response => response.json())
+        .then((titleData) => {
+          var id = titleData.imdbID;
+          while (id.charAt(0) === 't') {
+            id = id.substring(1);
+          }
+          if (!id) {
+            res.status(418).send({ message: 'id needed' });
+          } else {
+            fetch(`https://api.flixed.io/v1/movies/${id}?idType=imdb&apiKey=JvZosSdhe61qyfqx9cWtDmdng57IQHQJ`, {
+              method: 'GET',
+              headers: {
+                accept: 'application/json',
+              },
+            })
+              .then(flixedRes => flixedRes.json())
+              .then((data) => {
+                if (data.watchAvailability[0].directUrls[0] != null) {
+                  // cache the URL and associated metadata
+
+                  let insertSql = `INSERT INTO ContentMetaData(url, contentName, imdbID, streamingService, contentType) VALUES(?,?,?,'Netflix','MOVIE')`;
+
+                  connection.query(insertSql, [data.watchAvailability[0].directUrls[0], title, id], function (err, results, fields) {
+                    if (err) {
+                      console.log(err.message);
+                    }
+                  });
+
+                  connection.end(function (err) {
+                    if (err) {
+                      return console.log('error:' + err.message);
+                    }
+                    console.log('Close the database connection.');
+                  });
+                }
+
+                // send the data back
+                res.json({ url: `${data.watchAvailability[0].directUrls[0]}`, });
+              })
+              .catch((error) => {
+                res.status(418).send({ message: error.message });
+
+                connection.ping((err) => {
+                  if (!err) {
+                    connection.end(function (error) {
+                      if (error) {
+                        return console.log('error:' + err.message);
+                      }
+                      console.log('Close the database connection.');
+                    });
+                  }
+                })
+              });
+          }
         })
-          .then(flixedRes => flixedRes.json())
-          .then((data) => {
-            // cache the URL and associated metadata
-            // connection.connect(function (err) {
-            //   if (err) {
-            //     return console.error('error: ' + err.message);
-            //   }
+        .catch((error) => {
+          res.status(418).send({ message: error.message });
 
-            //   let sql = `SELECT url FROM ContentMetaData WHERE name=?`;
-
-            //   connection.query(sql, [title], function (err, results, fields) {
-            //     if (err) {
-            //       console.log(err.message);
-            //     }
-            //     console.log(results)
-            //   });
-
-            //   connection.end(function (err) {
-            //     if (err) {
-            //       return console.log('error:' + err.message);
-            //     }
-            //     console.log('Close the database connection.');
-            //   });
-            // });
-
-
-
-            // send the data back
-            res.json({ url: `${data.watchAvailability[0].directUrls[0]}`, });
+          connection.ping((err) => {
+            if (!err) {
+              connection.end(function (error) {
+                if (error) {
+                  return console.log('error:' + err.message);
+                }
+                console.log('Close the database connection.');
+              });
+            }
           })
-          .catch((error) => {
-            res.status(418).send({ message: error.message });
-          });
-      }
-    })
-    .catch((error) => {
-      res.status(418).send({ message: error.message });
-    });
+        });
+    }
+  });
 });
 
 app.get('/tvshow/:id', (req, res) => {
